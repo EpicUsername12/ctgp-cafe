c93d7ad93ec43d388927164a1df042b504df208c
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2024-01-08 17:13:00 +0100

    [ct-code] Implement CTDEF parsing/loading

--- source/patch/CourseRedirection.cpp
+++ source/patch/CourseRedirection.cpp
@@ -30,18 +30,137 @@ bool loadTextureInfo(nw::lyt::TextureInfo** textureInfo, const char* path, bool 
     return true;
 }
 
-bool CustomTrackManager::ParseDefinitionFile_V1(const char* path, bool panicOnFail) {
+CustomTrackManager::EParseError CustomTrackManager::ParseCTDFSection_V1(const CTDFSection_V1* data) {
+    if (data->magic != CTDFSection_V1::SectionMagic) {
+        return CTDF_WRONG_MAGIC;
+    }
+
+    if (data->ver_major > CustomTrackManager::MaximumMajorVersion) {
+        return CTDF_UNSUPPORTED_MAJOR_VERSION;
+    }
+
+    if (data->ver_minor > CustomTrackManager::MaximumMinorVersion) {
+        return CTDF_UNSUPPORTED_MINOR_VERSION;
+    }
+
+    this->m_ctdf = (CTDFSection_V1*)data;
+    return PARSE_OK;
+}
+
+CustomTrackManager::EParseError CustomTrackManager::ParseCTIFSection_V1(const CTIFSection_V1* data) {
+    if (data->magic != CTIFSection_V1::SectionMagic) {
+        return CTIF_WRONG_MAGIC;
+    }
+
+    this->m_ctif = (CTIFSection_V1*)data;
+    return PARSE_OK;
+}
+
+CustomTrackManager::EParseError CustomTrackManager::ParseCUP0Section_V1(const CUP0Section_V1* data) {
+    if (data->magic != CUP0Section_V1::SectionMagic) {
+        return CUP0_WRONG_MAGIC;
+    }
+
+    if (data->num_cups > MaximumCups) {
+        return CUP0_TOO_MANY_CUPS;
+    }
+
+    this->cups.resize(data->num_cups);
+    for (uint32_t i = 0; i < data->num_cups; i++) {
+        this->cups[i].icon_texture = nullptr;
+        memcpy(this->cups[i].name, data->cups[i].name, sizeof(this->cups[i].name));
+        memcpy(this->cups[i].icon, data->cups[i].icon, sizeof(this->cups[i].icon));
+
+        loadTextureInfo(&this->cups[i].icon_texture, this->cups[i].icon, true);
+
+        MessageRedirector::GetInstance()->addMessage(CUP0_MESSAGE_ID_CUP_NAME(i), this->cups[i].name, false);
+    }
+
+    this->m_cup0 = (CUP0Section_V1*)data;
+    return PARSE_OK;
+}
+
+CustomTrackManager::EParseError CustomTrackManager::ParseTRK0Section_V1(const TRK0Section_V1* data) {
+    if (data->magic != TRK0Section_V1::SectionMagic) {
+        return TRK0_WRONG_MAGIC;
+    }
+
+    int currentCup = 0;
+    Cup* cup = &this->cups[currentCup];
+    for (uint32_t i = 0; i < data->num_tracks; i++) {
+        if (this->cups[currentCup].tracks.size() >= 4) {
+            cup = &this->cups[++currentCup];
+        }
+
+        Track track;
+
+        track.flags = data->tracks[i].flags;
+        memcpy(track.retro_name, data->tracks[i].retro_name, sizeof(track.retro_name));
+        memcpy(track.name, data->tracks[i].name, sizeof(track.name));
+        memcpy(track.author, data->tracks[i].author, sizeof(track.author));
+        memcpy(track.directory, data->tracks[i].directory, sizeof(track.directory));
+        memcpy(track.track_pic, data->tracks[i].track_pic, sizeof(track.track_pic));
+
+        loadTextureInfo(&track.track_pic_texture, track.track_pic, true);
+
+        cup->tracks.push_back(track);
+
+        MessageRedirector::GetInstance()->addMessage(TRK0_MESSAGE_ID_AUTHOR_NAME(currentCup, i), cup->tracks[i].author, false);
+        MessageRedirector::GetInstance()->addMessage(TRK0_MESSAGE_ID_TRACK_NAME(currentCup, i), cup->tracks[i].name, false);
+        if (!(cup->tracks[i].flags & Track::TRACK_NO_RETRO_NAME)) {
+            MessageRedirector::GetInstance()->addMessage(TRK0_MESSAGE_ID_RETRO_NAME(currentCup, i), cup->tracks[i].retro_name, false);
+        }
+    }
+
+    this->m_trk0 = (TRK0Section_V1*)data;
+    return PARSE_OK;
+}
+
+bool CustomTrackManager::ParseDefinitionFile_V1(const char* path) {
     // Load the file
     const uint8_t* filedata = utils::fs::LoadFileEx(path, nullptr, 0x1000);
 
-    PANIC_UNIMPLEMENTED();
+    CTDFSection_V1* ctdf = (CTDFSection_V1*)(filedata);
+    EParseError error = this->ParseCTDFSection_V1(ctdf);
+    if (error != PARSE_OK) {
+        PANIC("Failed to parse CTDF section! (%s)\nPlease contact the developer of your distribution.\n", ParseErrorToString(error));
+    }
+
+    CTIFSection_V1* ctif = (CTIFSection_V1*)(filedata + sizeof(CTDFSection_V1));
+    error = this->ParseCTIFSection_V1(ctif);
+    if (error != PARSE_OK) {
+        PANIC("Failed to parse CTIF section! (%s)\nPlease contact the developer of your distribution.\n", ParseErrorToString(error));
+    }
+
+    CUP0Section_V1* cup0 = (CUP0Section_V1*)(filedata + sizeof(CTDFSection_V1) + sizeof(CTIFSection_V1));
+    error = this->ParseCUP0Section_V1(cup0);
+    if (error != PARSE_OK) {
+        PANIC("Failed to parse CUP0 section! (%s)\nPlease contact the developer of your distribution.\n", ParseErrorToString(error));
+    }
+
+    TRK0Section_V1* trk0 = (TRK0Section_V1*)((uint8_t*)cup0 + (sizeof(CUP0Section_V1) + sizeof(CUP0Section_V1::CupInfo) * cup0->num_cups));
+    error = this->ParseTRK0Section_V1(trk0);
+    if (error != PARSE_OK) {
+        PANIC("Failed to parse TRK0 section! (%s)\nPlease contact the developer of your distribution.\n", ParseErrorToString(error));
+    }
+
     return true;
 }
+
+static bool s_IsCourseRedirectionInitialized = false;
 
 void InitCourseRedirection() {
 
     // Load the CUP0 file if it exists
-    { PANIC_UNIMPLEMENTED(); }
+
+    if (s_IsCourseRedirectionInitialized)
+        return;
+
+    {
+        utils::ScopedHeapUsage heapUsage(GetExtensionHeap());
+        CustomTrackManager::GetInstance()->ParseDefinitionFile_V1(CTDEF_BIN_FILEPATH);
+        s_IsCourseRedirectionInitialized = true;
+    }
 }
 
 sead::ExpHeap* GetExtensionHeap() {
--- source/patch/CourseRedirection.h
+++ source/patch/CourseRedirection.h
@@ -9,14 +9,17 @@
 #include <heap/seadExpHeap.h>
 #include <mk8/nw/lyt/Texture.h>
 
+#include "utils/ScopedHeapUsage.h"
 #include "utils/Filesystem.h"
 #include "MessageRedirection.h"
 
 namespace ctgp {
 
+#define CTDEF_BIN_FILEPATH "ctdef.bin"
 #define CUP0_MESSAGE_ID_CUP_NAME(fileCupIdx) (13337 + fileCupIdx)
-#define CUP0_MESSAGE_ID_TRACK_NAME(fileCupIdx, trackIdx) (15337 + (fileCupIdx * 4) + trackIdx)
-#define CUP0_MESSAGE_ID_RETRO_NAME(fileCupIdx, trackIdx) (15937 + (fileCupIdx * 4) + trackIdx)
+#define TRK0_MESSAGE_ID_TRACK_NAME(fileCupIdx, trackIdx) (15337 + (fileCupIdx * 4) + trackIdx)
+#define TRK0_MESSAGE_ID_RETRO_NAME(fileCupIdx, trackIdx) (15937 + (fileCupIdx * 4) + trackIdx)
+#define TRK0_MESSAGE_ID_AUTHOR_NAME(fileCupIdx, trackIdx) (16937 + (fileCupIdx * 4) + trackIdx)
 
 void InitCourseRedirection();
 
@@ -46,29 +49,110 @@ struct CUP0Section_V1 {
     static const uint32_t SectionMagic = 'CUP0';
 
     uint32_t magic;
-    wchar_t name[48];
-    wchar_t version[16];
-    wchar_t author[96];
-    wchar_t description[512];
+    uint32_t num_cups;
+
+    struct CupInfo {
+        wchar_t name[16];
+        char icon[128];
+    };
+
+    CupInfo cups[];
+};
+
+struct TRK0Section_V1 {
+
+    static const uint32_t SectionMagic = 'TRK0';
+
+    uint32_t magic;
+    uint32_t num_tracks;
+
+    struct TrackInfo {
+
+        enum EFlags {
+            TRACK_NO_RETRO_NAME = 1 << 0,
+        };
+
+        EFlags flags;
+        wchar_t retro_name[6];
+        wchar_t name[20];
+        wchar_t author[20];
+        char directory[32];
+        char track_pic[128];
+    } __attribute__((aligned(4)));
+
+    TrackInfo tracks[];
 };
 
 class CustomTrackManager {
 
     static const uint16_t MaximumMajorVersion = 1;
     static const uint16_t MaximumMinorVersion = 0;
+    static const uint32_t MaximumCups = 24;
+    static const uint32_t MaximumTracks = 4 * MaximumCups;
+
+    enum EParseError {
+        PARSE_OK = 0,
+        CTDF_WRONG_MAGIC,
+        CTDF_UNSUPPORTED_MAJOR_VERSION,
+        CTDF_UNSUPPORTED_MINOR_VERSION,
+        CTIF_WRONG_MAGIC,
+        CUP0_WRONG_MAGIC,
+        CUP0_TOO_MANY_CUPS,
+        TRK0_WRONG_MAGIC,
+    };
+
+    EParseError ParseCTDFSection_V1(const CTDFSection_V1* data);
+    EParseError ParseCTIFSection_V1(const CTIFSection_V1* data);
+    EParseError ParseCUP0Section_V1(const CUP0Section_V1* data);
+    EParseError ParseTRK0Section_V1(const TRK0Section_V1* data);
 
   public:
-    CustomTrackManager() {
+    CustomTrackManager() : m_ctdf(nullptr), m_ctif(nullptr), m_cup0(nullptr), m_trk0(nullptr) {
     }
 
-    bool ParseDefinitionFile_V1(const char* path, bool panicOnFail = true);
+    bool ParseDefinitionFile_V1(const char* path);
 
     CTDFSection_V1* m_ctdf;
     CTIFSection_V1* m_ctif;
     CUP0Section_V1* m_cup0;
+    TRK0Section_V1* m_trk0;
+
+    struct Track : public TRK0Section_V1::TrackInfo {
+        nw::lyt::TextureInfo* track_pic_texture;
+    };
+
+    struct Cup : public CUP0Section_V1::CupInfo {
+        nw::lyt::TextureInfo* icon_texture;
+        std::vector<Track> tracks;
+    };
+
+    std::vector<Cup> cups;
 
     static CustomTrackManager* s_Instance;
     static CustomTrackManager* GetInstance();
+
+    static const char* ParseErrorToString(EParseError error) {
+        switch (error) {
+            case PARSE_OK:
+                return "No error";
+            case CTDF_WRONG_MAGIC:
+                return "CTDF wrong magic";
+            case CTDF_UNSUPPORTED_MAJOR_VERSION:
+                return "CTDF unsupported major version";
+            case CTDF_UNSUPPORTED_MINOR_VERSION:
+                return "CTDF unsupported minor version";
+            case CTIF_WRONG_MAGIC:
+                return "CTIF wrong magic";
+            case CUP0_WRONG_MAGIC:
+                return "CUP0 wrong magic";
+            case CUP0_TOO_MANY_CUPS:
+                return "CUP0 too many cups";
+            case TRK0_WRONG_MAGIC:
+                return "TRK0 wrong magic";
+            default:
+                return "Unknown error";
+        }
+    }
 };
 
 extern sead::ExpHeap* g_CTGPHeap;
